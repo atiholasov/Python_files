a550d75a5d2a9fe074d171de09c5002ef61be290
atiholasov <alexey.tiholasov@apstecsystems.com>
2023-03-03 20:55:56 +0300

    many updates

--- Spectro_fotometr_tk.py
+++ Spectro_fotometr_tk.py
@@ -2,8 +2,10 @@
 # needed to: pip install openpyxl
 
 import tkinter as tk
+import numpy as np
 import pandas as pd
 
+# Graphics window
 """
 path_work_file, path_etalon_file = "", ""
 
@@ -38,20 +40,51 @@ output_btn.pack(pady=10)
 win.mainloop()
 """
 
-
-
-
-
-
-
-
-# Working with data
+# Reading files
 
 #path_etalon_file = r"C:\Users\Ivan\Desktop\Alexey_Tiholasov\Script_for_specrofotometr\Data\2022-06-21_Al.xlsx"
 #path_work_file = r"C:\Users\Ivan\Desktop\Alexey_Tiholasov\Script_for_specrofotometr\Data\Ag на кремнии.xlsx"
 
-etalon_data = pd.read_excel(r"a_2022-06-21_Al.xlsx", header=None)
-#data_for_work = pd.read_excel(path_work_file, header=None)
+path_etalon_file = r"a_2022-06-21_Al.xlsx"
+path_work_file = r"Ag на кремние.xlsx"
 
-print(etalon_data)
-#print(data_for_work)
+etalon_data = pd.read_excel(path_etalon_file, header=None)
+data_for_work = pd.read_excel(path_work_file, header=None)
+
+# Working with data
+
+data_for_work.iloc[:, 0] = (round(data_for_work.iloc[:, 0]))
+
+new_first_column = np.array(range(etalon_data.iloc[0, 0], etalon_data.iloc[-1, 0]+1))
+len_new_first_column = len(new_first_column)
+new_second_column = np.ones(len(new_first_column))
+new_etalon_data = pd.DataFrame(np.column_stack([new_first_column, new_second_column]))
+new_etalon_data = new_etalon_data.astype(int)
+
+etalon_data_dict = {}
+for index in etalon_data.index:
+    etalon_data_dict[etalon_data.iloc[index, 0]] = etalon_data.iloc[index, 1]
+
+for index in new_etalon_data.index:
+    if new_etalon_data.iloc[index, 0] in etalon_data_dict:
+        new_etalon_data.iloc[index, 1] = etalon_data_dict[new_etalon_data.iloc[index, 0]]
+    else:
+        new_etalon_data.iloc[index, 1] = np.nan
+
+# new_etalon_data - хороший, но тупо пременить интерполяцию нельзя, она возьмет только первый и последний
+# элементы. Нужно разбить на интервалы и их интерполировать, а затем склеить.
+# Пробовал что то такое ниже
+
+
+#print(new_etalon_data.head(31))
+
+#new_etalon_data = new_etalon_data.iloc[:, 1].interpolate()
+#print(new_etalon_data.head(31))
+
+a = [[95.1, np.nan, np.nan, 93.2],[93.2, np.nan, np.nan, 91.5]]
+b = pd.Series(0)
+for elem in a:
+    aa = pd.Series(elem)
+    aa = aa.interpolate()
+    b = b.conco + aa
+print(b)
